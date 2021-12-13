1e5d62d0dc07c1d5da3d3506d4b0cc34b074c4ef
Tarik <tarikmerrylees@gmail.com>
2021-12-13 19:19:08 +0000

    main: minor changes

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script>
   import Header from "./components/global/HeaderGlobal.svelte";
-  import HeroHome from "./components/home/Hero_home.svelte";
+  import HeroHome from "./components/home/hero_home/Hero_home.svelte";
 </script>
 
 <main>
