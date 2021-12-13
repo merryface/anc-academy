5ef131da263ae6751d7702a4a2ed033671f2bdd2
Tarik <tarikmerrylees@gmail.com>
2021-12-13 05:38:04 +0000

    feature/hero: structured view and added font families

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,11 @@
 <script>
   import Header from "./components/global/HeaderGlobal.svelte";
+  import HeroHome from "./components/home/Hero_home.svelte";
 </script>
 
 <main>
   <Header />
-
+  <HeroHome />
 </main>
 
 <style>
