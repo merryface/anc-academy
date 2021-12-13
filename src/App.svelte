d3c43fe8da63a8798d22f87419dec31de6ab27cd
Tarik <tarikmerrylees@gmail.com>
2021-12-13 19:49:01 +0000

    feature/learningFromPilots: markup structure

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,14 @@
 <script>
   import Header from "./components/global/HeaderGlobal.svelte";
   import HeroHome from "./components/home/hero_home/Hero_home.svelte";
+  import LearningFromPilots from "./components/home/learningFromPilots/LearningFromPilots.svelte";
+  
 </script>
 
 <main>
   <Header />
   <HeroHome />
+  <LearningFromPilots />
 </main>
 
 <style>
