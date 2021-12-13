5b4359ee9b1891404c38d45d620fa76ddd37a2e4
Tarik <tarikmerrylees@gmail.com>
2021-12-13 22:19:35 +0000

    feature/learningFromPilots: mobile styled

--- src/components/home/learningFromPilots/LearningFromPilots.svelte
+++ src/components/home/learningFromPilots/LearningFromPilots.svelte
@@ -1,6 +1,6 @@
 <script>
   import FaChevronRight from 'svelte-icons/fa/FaChevronRight.svelte'
-  const text = "A.N.C has one simple goal in mind, to provide high quality and affordable flight training in a convenient form. We are here to show you how to operate your simulator aircraft to the highest level of accuracy. We have two decades of flight experience to share with you, and we can’t wait to do so. Using modern online tools and your personal simulator, we can accurately walk you through procedures, explain theory, and guide you through exercises to make you a better pilot."
+  const text = "<p>A.N.C has one simple goal in mind, to provide high quality and affordable flight training in a convenient form. We are here to show you how to operate your simulator aircraft to the highest level of accuracy. We have two decades of flight experience to share with you, and we can’t wait to do so. <br><br> Using modern online tools and your personal simulator, we can accurately walk you through procedures, explain theory, and guide you through exercises to make you a better pilot.<p>"
 </script>
 
 <section class="LearningFromPilots">
@@ -11,7 +11,7 @@
       <a href="/about" class="LearningFromPilots__link">Learn more about A.N.C Academy <div class="icon"><FaChevronRight /></div></a>
     </div>
     <div class="LearningFromPilots__text">
-      {text}
+      <div class="LearningFromPilots__textContent">{@html text}</div> 
     </div>
   </div>
 </section>
