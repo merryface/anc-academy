ecebae2d68ec8656fac18779f2113a160a01c9bb
Tarik <tarikmerrylees@gmail.com>
2021-12-14 22:14:05 +0000

    freeSeminar: structured view and set background images

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
   import Header from "./components/global/HeaderGlobal.svelte";
+  import FreeSeminars from "./components/home/freeSeminars/FreeSeminars.svelte";
   import HeroHome from "./components/home/hero_home/Hero_home.svelte";
   import LearningFromPilots from "./components/home/learningFromPilots/LearningFromPilots.svelte";
   
@@ -9,7 +10,5 @@
   <Header />
   <HeroHome />
   <LearningFromPilots />
+  <FreeSeminars />
 </main>
-
-<style>
-</style>
