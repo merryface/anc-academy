d3cbcaceafc1ccc3bc47a9aff6fd4f7ca239497e
Tarik <tarikmerrylees@gmail.com>
2021-12-15 21:06:16 +0000

    testimonials: pre-branch change commit

---
+++ src/components/home/testimonials/Testimonials.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { testimonials } from './testimonials';
+  let testimonialShowIndex = 0;
+  $: console.log(testimonialShowIndex);
+  
+</script>
+
+<section class="Testimonials">
+  <div class="Testimonials__container">
+    <h2 class="Testimonials__title">Testimonials</h2>
+
+    <div class="Testimoials__carousel">
+      {#each testimonials as {text, name, title}}
+      <figure class="Testimonials__testimonial">
+        <blockquote class="Testimonials__text">{text}</blockquote>
+        <figcaption class="Testimonials__authorName">{name}</figcaption>
+        <cite class="Testimonials__authortitle">{title}</cite>
+      </figure>
+      {/each}
+    </div>
+  </div>
+</section>
+
+<style type="text/scss">
+  @import './testimonials.scss';
+</style>
