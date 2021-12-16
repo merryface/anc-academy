4b046ef6166e9999de3bb4836f3dfe967a378fac
Tarik <tarikmerrylees@gmail.com>
2021-12-16 22:12:06 +0000

    testimonials: added pagination elements

--- src/components/home/testimonials/Testimonials.svelte
+++ src/components/home/testimonials/Testimonials.svelte
@@ -2,9 +2,11 @@
   import { testimonials } from './testimonials';
   import FaQuoteLeft from 'svelte-icons/fa/FaQuoteLeft.svelte';
   import FaQuoteRight from 'svelte-icons/fa/FaQuoteRight.svelte';
+  import FaChevronLeft from 'svelte-icons/fa/FaChevronLeft.svelte'
+  import FaChevronRight from 'svelte-icons/fa/FaChevronRight.svelte'
 
   let testimonialShowIndex = 0;
-  $: console.log(testimonialShowIndex);
+  let left = id => (id - testimonialShowIndex) * 100;
   
 </script>
 
@@ -14,22 +16,30 @@
 
     <div class="Testimoials__carousel">
       {#each testimonials as {id, text, name, title}}
-      <figure class="Testimonials__testimonial" class:show={id === testimonialShowIndex}>
-        <div class="icon icon-top">
-          <FaQuoteLeft />
-        </div>
-
-        <blockquote class="Testimonials__text">
-          {text}
-        </blockquote>
-
-        <div class="icon icon-bottom">
-          <FaQuoteRight />
-        </div>
-        <figcaption class="Testimonials__authorName">{name}</figcaption>
-        <cite class="Testimonials__authorTitle">{title}</cite>
-      </figure>
+      <div class="carouselItem" style="left: {left(id)}">
+        <figure class="Testimonials__testimonial" class:show={id === testimonialShowIndex}>
+          <div class="icon icon-top">
+            <FaQuoteLeft />
+          </div>
+          
+          <blockquote class="Testimonials__text">
+            {text}
+          </blockquote>
+          
+          <div class="icon icon-bottom">
+            <FaQuoteRight />
+          </div>
+          <figcaption class="Testimonials__authorName">{name}</figcaption>
+          <cite class="Testimonials__authorTitle">{title}</cite>
+        </figure>
+      </div>
       {/each}
+    </div>
+
+    <div class="Testimonials__navigation">
+      <div class="icon left"><FaChevronLeft /></div>
+      <div class="Testimonials__pagination">{testimonialShowIndex+1}/{testimonials.length}</div>
+      <div class="icon right"><FaChevronRight /></div>
     </div>
   </div>
 </section>
