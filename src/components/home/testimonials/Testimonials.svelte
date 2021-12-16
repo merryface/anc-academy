c60b4593c2b0e4566eeba7ce2d2f6550a9c2292e
Tarik <tarikmerrylees@gmail.com>
2021-12-16 22:50:29 +0000

    testimonials: carousel functions

--- src/components/home/testimonials/Testimonials.svelte
+++ src/components/home/testimonials/Testimonials.svelte
@@ -6,8 +6,18 @@
   import FaChevronRight from 'svelte-icons/fa/FaChevronRight.svelte'
 
   let testimonialShowIndex = 0;
-  let left = id => (id - testimonialShowIndex) * 100;
   
+  let left = id => (id - testimonialShowIndex) * 100;
+
+  let prevItem = () => {
+    if (testimonialShowIndex === 0) testimonialShowIndex = testimonials.length - 1;
+    else testimonialShowIndex -= 1;
+  }
+  
+  let nextItem = () => {
+    if (testimonialShowIndex === testimonials.length - 1) testimonialShowIndex = 0;
+    else testimonialShowIndex += 1;
+  }
 </script>
 
 <section class="Testimonials">
@@ -37,9 +47,9 @@
     </div>
 
     <div class="Testimonials__navigation">
-      <div class="icon left"><FaChevronLeft /></div>
+      <div class="icon left" on:click={prevItem}><FaChevronLeft /></div>
       <div class="Testimonials__pagination">{testimonialShowIndex+1}/{testimonials.length}</div>
-      <div class="icon right"><FaChevronRight /></div>
+      <div class="icon right" on:click={nextItem}><FaChevronRight /></div>
     </div>
   </div>
 </section>
