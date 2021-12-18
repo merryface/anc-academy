41625d4583e631116c8d241e0e581f8d3f9fd2d2
Tarik <tarikmerrylees@gmail.com>
2021-12-18 15:45:25 +0000

    footer: mobile styled

--- src/components/global/footer/Footer.svelte
+++ src/components/global/footer/Footer.svelte
@@ -9,7 +9,14 @@
     <nav class="Footer__nav">
       {#each footerNav as {id, label, url}}
       <a href="{url}" class="Footer__navItem">{label}</a>
+      {#if id !== footerNav.length - 1}
+      <span class="Footer__navItem pipe">|</span>
+      {/if}
       {/each}
     </nav>
   </div>
 </footer>
+
+<style type="text/scss">
+  @import './footer.scss';
+</style>
