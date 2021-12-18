6a5f5dfdde54ea025c694067c99eecfcc921da8a
Tarik <tarikmerrylees@gmail.com>
2021-12-18 12:52:32 +0000

    contactUs: mobile styled

--- src/components/home/contactUs/ContactUs.svelte
+++ src/components/home/contactUs/ContactUs.svelte
@@ -4,16 +4,19 @@
   import FaInstagram from 'svelte-icons/fa/FaInstagram.svelte'
 
   const email= "admin@anc.co.uk";
+  const youtube = "https://www.youtube.com/channel/UCjkotIU0LsTjMTkYap6cjvw";
+  const facebook = "https://www.facebook.com/home.php";
+  const instagram = "https://www.instagram.com/";
 </script>
 
 <section class="ContactUs">
   <div class="ContactUs__container">
     <h2 class="ContactUs__title">Contact</h2>
-    <a href="mailto:{email}" class="ContactUs__email">{email}</a>
+    <a href="mailto:{email}" class="ContactUs__email">Email: {email}</a>
     <div class="ContactUs__socialMedia">
-      <div class="ContactUs__icon"><FaYoutube /></div>
-      <div class="ContactUs__icon"><FaFacebook /></div>
-      <div class="ContactUs__icon"><FaInstagram /></div>
+      <a href="{youtube}" target="_blank"><div class="ContactUs__icon"><FaYoutube /></div></a>
+      <a href="{facebook}" target="_blank"><div class="ContactUs__icon"><FaFacebook /></div></a>
+      <a href="{instagram}" target="_blank"><div class="ContactUs__icon"><FaInstagram /></div></a>
     </div>
   </div>
 </section>
