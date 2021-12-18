79a1613595ba4732d771d94c55b7d14e8700d007
Tarik <tarikmerrylees@gmail.com>
2021-12-18 12:52:32 +0000

    contactUs: mobile styled

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,11 @@
 <script>
   import Header from "./components/global/HeaderGlobal.svelte";
+  import ContactUs from "./components/home/contactUs/ContactUs.svelte";
   import FreeSeminars from "./components/home/freeSeminars/FreeSeminars.svelte";
   import HeroHome from "./components/home/hero_home/Hero_home.svelte";
   import LearningFromPilots from "./components/home/learningFromPilots/LearningFromPilots.svelte";
   import Testimonials from "./components/home/testimonials/Testimonials.svelte";
+  
 </script>
 
 <main>
@@ -12,4 +14,5 @@
   <LearningFromPilots />
   <FreeSeminars />
   <Testimonials />
+  <ContactUs />
 </main>
