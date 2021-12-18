447e99239115942f24a61aea0735f2786d8546f5
Tarik <tarikmerrylees@gmail.com>
2021-12-18 15:04:05 +0000

    footer: view structured

--- src/components/global/footer/Footer.svelte
+++ src/components/global/footer/Footer.svelte
@@ -1,3 +1,15 @@
-<script></script>
+<script>
+  import { footerNav } from './footer.js';
 
-<footer class="Footer"></footer>
+</script>
+
+<footer class="Footer">
+  <div class="Footer__container">
+    <p class="Footer__copyright">© 2021, A.N.C Academy</p>
+    <nav class="Footer__nav">
+      {#each footerNav as {id, label, url}}
+      <a href="{url}" class="Footer__navItem">{label}</a>
+      {/each}
+    </nav>
+  </div>
+</footer>
