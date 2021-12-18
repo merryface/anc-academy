c8034b542bba877754c7ea8dfe54d13f31f1d621
Tarik <tarikmerrylees@gmail.com>
2021-12-18 15:04:05 +0000

    footer: view structured

---
+++ src/components/global/footer/footer.js
@@ -0,0 +1,17 @@
+export const footerNav = [
+  {
+    id: 0,
+    label: "About",
+    url: "/about",
+  },
+  {
+    id: 1,
+    label: "Terms & Conditions",
+    url: "/terms-and-conditions",
+  },
+  {
+    id: 2,
+    label: "Join Us",
+    url: "/join-us",
+  },
+] 
