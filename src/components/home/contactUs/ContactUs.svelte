f058ccacd961954526c654b1f630d07a58d1e094
Tarik <tarikmerrylees@gmail.com>
2021-12-18 12:36:02 +0000

    contactUs: view strucutred

---
+++ src/components/home/contactUs/ContactUs.svelte
@@ -0,0 +1,23 @@
+<script>
+  import FaYoutube from 'svelte-icons/fa/FaYoutube.svelte'
+  import FaFacebook from 'svelte-icons/fa/FaFacebook.svelte'
+  import FaInstagram from 'svelte-icons/fa/FaInstagram.svelte'
+
+  const email= "admin@anc.co.uk";
+</script>
+
+<section class="ContactUs">
+  <div class="ContactUs__container">
+    <h2 class="ContactUs__title">Contact</h2>
+    <a href="mailto:{email}" class="ContactUs__email">{email}</a>
+    <div class="ContactUs__socialMedia">
+      <div class="ContactUs__icon"><FaYoutube /></div>
+      <div class="ContactUs__icon"><FaFacebook /></div>
+      <div class="ContactUs__icon"><FaInstagram /></div>
+    </div>
+  </div>
+</section>
+
+<style type="text/scss">
+  @import './contact_us.scss';
+</style>
