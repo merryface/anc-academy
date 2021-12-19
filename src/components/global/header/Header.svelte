8e57778369f9fecbf8e5a0a0849e25a7ceb16c3e
Tarik <tarikmerrylees@gmail.com>
2021-12-19 20:50:32 +0000

    routing: routing installed

--- src/components/global/header/Header.svelte
+++ src/components/global/header/Header.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { Router, Link } from 'svelte-routing';
   let open = false;
   const toggleMenu = () => open = !open;
   const openClass = "HeaderMobile__navMenu-active";
@@ -10,13 +11,15 @@
 <header class="Header">
   <div class="Header__container">
     <a href="/"><img class="Header__logo" {src} alt="white ANC logo"></a>
-    <ul class="Header__navItems">
+    <Router>
+    <nav class="Header__navItems">
       <li class="Header__navItem"><a href="/">Home</a></li>
-      <li class="Header__navItem"><a href="/about">About</a></li>
+      <Link class="Header__navItem" to="about">About</Link>
       <li class="Header__navItem"><a href="/pricing">Pricing</a></li>
       <li class="Header__navItem"><a href="/seminar">Seminar</a></li>
       <li class="Header__navItem"><a href="/book">Book</a></li>
-    </ul>
+    </nav>
+    </Router>
   </div>
 </header>
 
---
+++ src/routes/index.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { Router, Route } from 'svelte-routing';
+  import Home from "../components/home/Home.svelte";
+  import About from '../components/About/About.svelte';
+  
+  export let url = '';
+</script>
+
+<Router {url}>
+  <div>
+    <Route path="/" component={Home} />
+    <Route path="about" component={About} />
+  </div>
+</Router>
