74cc262c48c8f4d34ea0865f01d122434d663464
Tarik <tarikmerrylees@gmail.com>
2021-12-19 21:58:41 +0000

    routing: routing set up

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,7 +2,7 @@
   import { Router, Route } from 'svelte-routing';
   import Home from "../components/home/Home.svelte";
   import About from '../components/About/About.svelte';
-  
+  import NotFound from './404.svelte';
   export let url = '';
 </script>
 
@@ -10,5 +10,6 @@
   <div>
     <Route path="/" component={Home} />
     <Route path="about" component={About} />
+    <Route component={NotFound} />
   </div>
 </Router>
