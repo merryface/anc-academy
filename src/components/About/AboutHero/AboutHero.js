0fae68d5ad8ee17df40684c569020ca3179d1991
Tarik <tarikmerrylees@gmail.com>
2021-12-20 22:54:39 +0000

    hero: styled and responsive

---
+++ src/components/About/AboutHero/AboutHero.js
@@ -0,0 +1,8 @@
+export const content = {
+  title: "About ANC Academy", 
+  subtitle: "Welcome to the best online flight training school you can find for flight simulator.",
+  image: {
+    url: "./assets/da62Hangar.jpg",
+    alt: "MSFS Da62 in Hangar",
+  }
+}
---
+++ src/components/About/AboutHero/AboutHero.svelte
@@ -0,0 +1,17 @@
+<script>
+  import {content} from './AboutHero';
+</script>
+
+<section class="AboutHero">
+  <div class="AboutHero__container">
+    <div class="AboutHero__text">
+      <h1 class="AboutHero__title">{content.title}</h1>
+      <p class="AboutHero__subtitle">{content.subtitle}</p>
+    </div>
+    <img src={content.image.url} alt={content.image.alt} class="AboutHero__image">
+  </div>
+</section>
+
+<style type="text/scss">
+  @import './about_hero.scss';
+</style>
