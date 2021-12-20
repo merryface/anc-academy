4c7b13cebcca48ef1f8417de3a6a2d67e2ce0e46
Tarik <tarikmerrylees@gmail.com>
2021-12-20 09:34:49 +0000

    refactor: free seminars and contact us

---
+++ src/components/home/contactUs/ContactUs.js
@@ -0,0 +1,19 @@
+export const title = "Contact";
+export const email = "admin@anc.co.uk"
+export const socialMediaUrl = [
+  {
+  id: 0,
+    name: "youtube",
+    url: "https://www.youtube.com/channel/UCjkotIU0LsTjMTkYap6cjvw",
+  },
+  {
+    id: 1,
+    name: "facebook",
+    url: "https://www.facebook.com/AviateNavigateCommunicateAcademy/",
+  },
+  {
+    id: 2,
+    name: "instagram",
+    url: "https://www.instagram.com/a.n.c_academy/",
+  },
+]
