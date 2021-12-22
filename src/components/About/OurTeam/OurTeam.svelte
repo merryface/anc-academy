875f52fe70d6633d6396474946979ae43c955e17
Tarik <tarikmerrylees@gmail.com>
2021-12-22 09:03:52 +0000

    ourTeam: markup and content added

---
+++ src/components/About/OurTeam/OurTeam.svelte
@@ -0,0 +1,25 @@
+<script>
+  import {content} from './OurTeam';
+</script>
+
+<section class="OurTeam">
+  <div class="OurTeam__container">
+    <h2 class="OurTeam__title">{content.title}</h2>
+    <p class="OurTeam__subtitle">{content.subtitle}</p>
+
+    <div class="OurTeam__teamMembers">
+      {#each content.teamMembers as teamMember}
+        <div class="OurTeam__teamMember">
+          <img src={teamMember.image.url} alt={teamMember.image.url} class="OurTeam__memberImage">
+          <h3 class="teamMember__name">{teamMember.name}</h3>
+          <p class="OurTeam__bio">{teamMember.bio}</p>
+        </div>
+      {/each}
+
+    </div>
+  </div>
+</section>
+
+<style type="text/scss">
+  @import './our-team.scss';
+</style>
