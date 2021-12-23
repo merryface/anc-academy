d19f3c0520affb038c0f3a01ab3494ed5810738b
Tarik <tarikmerrylees@gmail.com>
2021-12-23 17:36:11 +0000

    seminarsHero: structured and mobile structured

---
+++ src/components/Seminars/SeminarsHero/SeminarsHero.svelte
@@ -0,0 +1,16 @@
+<script>
+  import {content} from './SeminarsHero';
+</script>
+
+<section class="FreeSeminars" style="background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url({content.imageUrl})">
+  <div class="FreeSeminars__container">
+    <div class="FreeSeminars__text">
+      <h1 class="FreeSeminars__title">{content.title}</h1>
+      <p class="FreeSeminars__subtitle">{content.subtitle}</p>
+    </div>
+  </div>
+</section>
+
+<style type="text/scss">
+  @import './seminars-hero.scss';
+</style>
