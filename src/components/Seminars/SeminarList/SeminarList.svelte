f1fb41ba486b612e02e008a539c9ec0b86c08d38
Tarik <tarikmerrylees@gmail.com>
2021-12-26 16:27:44 +0000

    seminarList: wip

---
+++ src/components/Seminars/SeminarList/SeminarList.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { content } from './SeminarList';
+</script>
+
+<section class="SeminarList">
+  <div class="SeminarList__container">
+    <div class="SeminarList__text">
+      <h2 class="SeminarList__title">{content.title}</h2>
+      <p class="SeminarList__summary">content.summary</p>
+    </div>
+
+    <div class="SeminarList__seminars">
+      {#each content.seminars as seminar}
+      <div class="SeminarList__seminar">
+        <div class="SeminarList__seminarText">
+          <h3 class="SeminarList__seminarTitle">{seminar.title}</h3>
+          <p class="SeminarList__seminarSummary">{seminar.summary}</p>
+        </div>
+        <img src={seminar.imageSrc} alt={seminar.imageAlt} class="SeminarList__seminarImage">
+      </div>
+      {/each}
+    </div>
+  </div>
+</section>
