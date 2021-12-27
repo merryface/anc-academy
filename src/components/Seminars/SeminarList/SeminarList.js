a8dad94091e9744fa2977dafd8627510e8338d1c
Tarik <tarikmerrylees@gmail.com>
2021-12-27 12:46:23 +0000

    seminarList: content added to page

--- src/components/Seminars/SeminarList/SeminarList.js
+++ src/components/Seminars/SeminarList/SeminarList.js
@@ -1,4 +1,24 @@
 export const content = {
-  title: "",
-  
+  title: "Overview",
+  summary: "The seminars are suitable for all levels of experience, and no prior knowledge is required. The seminars last from two to three hours.",
+  seminars: [
+    {
+      title: "IFR Flying introduction",
+      summary: "Students in these seminars learn the skills to practice in order to manually fly on instruments accurately, track VORs and shoot an ILS approach the correct way. This seminar will give them an overview of the skills we can teach flight simulator pilots.",
+      imageSrc: "./assets/seminarIMages/G1000-closeup_tablet.jpg",
+      imageAlt: "G1000 Close Up",
+    },
+    {
+      title: "VFR Flying introduction",
+      summary: "VFR navigation is how every pilot start learning how to travel from one point to another. Students learn how to navigate using nothing more than charts, a compass, and a clock. Students apply real world procedures in order to successfully find their destination.",
+      imageSrc: "./assets/seminarIMages/rainVFR_tablet.jpg",
+      imageAlt: "MSFS Aircraft flying close to rainy weather",
+    },
+    {
+      title: "A320 Flying Introduction",
+      summary: "Students captain an Airbus A320 as they get used to its unique fly by wire system, and learn how to operate its advanced autopilot system. After this seminar, students will gain a better understanding of the Airbus philosophy.",
+      imageSrc: "./assets/seminarIMages/FCU-closeup_tablet.jpg",
+      imageAlt: "A320 FCU at dusk",
+    },
+  ]
 }
