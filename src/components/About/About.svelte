565e12d98bd7f2b500b1c0c8fddb291f2c2445f8
Tarik <tarikmerrylees@gmail.com>
2021-12-27 12:29:24 +0000

    merge

--- src/components/About/About.svelte
+++ src/components/About/About.svelte
@@ -3,7 +3,7 @@
   import AncaWay from "./AncaWay/AncaWay.svelte";
   import Ourservices from "./OurServices/Ourservices.svelte";
   import OurTeam from "./OurTeam/OurTeam.svelte";
-  import WhoOurClientsAre from "./OurClientsAre/WhoOurClientsAre.svelte";
+  import WhoOurClientsAre from "./OurClients/WhoOurClientsAre.svelte";
 </script>
 
 <main>
