2b9509f58bb55a105564f9c2affc7369f879da51
Tarik <tarikmerrylees@gmail.com>
2022-01-21 22:07:46 +0000

    textContent: markup added

---
+++ src/components/Pricing/TextContent/TextContent.svelte
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+<section class="TextContent">
+  <div class="TextContent__container">
+    <div class="TextContent__nextSeminar">
+      <p class="TextContent__text"></p>
+      <a href="/" class="TextContent__seminarButton">{"yo"}</a>
+    </div>
+    <div class="TextContent__nextSeminar">
+      <p class="TextContent__text"></p>
+      <a href="/" class="TextContent__lessonButton">{"yo"}</a>
+    </div>
+  </div>
+</section>
