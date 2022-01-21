b5ac9bed286b915314d30a5770a9679a0609849b
Tarik <tarikmerrylees@gmail.com>
2022-01-21 22:54:38 +0000

    WIP

--- src/components/Pricing/Pricing.svelte
+++ src/components/Pricing/Pricing.svelte
@@ -1,7 +1,9 @@
 <script>
   import PricingHero from "./PricingHero/PricingHero.svelte";
   import Rates from "./Rates/Rates.svelte";
+  import TextContent from "./TextContent/TextContent.svelte";
 </script>
 
 <PricingHero />
 <Rates />
+<TextContent />
--- src/components/Pricing/TextContent/TextContent.svelte
+++ src/components/Pricing/TextContent/TextContent.svelte
@@ -1,16 +1,25 @@
 <script>
-
+  import { content } from './TextContent.js';
+  import { Link } from 'svelte-routing';
+  import IoIosAirplane from 'svelte-icons/io/IoIosAirplane.svelte'
 </script>
 
 <section class="TextContent">
   <div class="TextContent__container">
     <div class="TextContent__nextSeminar">
-      <p class="TextContent__text"></p>
-      <a href="/" class="TextContent__seminarButton">{"yo"}</a>
+      <p class="TextContent__text">{content.seminar.text}</p>
+      <Link to={content.seminar.url} class="TextContent__seminarButton">
+        {content.seminar.label}
+        <div class="icon"><IoIosAirplane /></div>
+      </Link>
     </div>
-    <div class="TextContent__nextSeminar">
-      <p class="TextContent__text"></p>
-      <a href="/" class="TextContent__lessonButton">{"yo"}</a>
+    <div class="TextContent__lesson">
+      <p class="TextContent__text">{content.lesson.text}</p>
+      <a href="{content.lesson.url}" class="TextContent__lessonButton">{content.lesson.url}</a>
     </div>
   </div>
 </section>
+
+<style lang="scss">
+  @import './textContent.scss';
+</style>
