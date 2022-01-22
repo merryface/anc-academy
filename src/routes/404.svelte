7567a4bd86b25355256621ed940325214e5d35cd
Tarik <tarikmerrylees@gmail.com>
2022-01-22 10:32:43 +0000

    404 styled and populated

--- src/routes/404.svelte
+++ src/routes/404.svelte
@@ -1,6 +1,23 @@
+<script>
+import { Link } from "svelte-routing";
+</script>
+
 <section class="FourOhFour">
-  <h1 class="FourOhFour__title">Page Not Found</h1>
-  <h2 class="FourOhFour__subtitle">Looks like you got a little lost.</h2>
-  <p class="FourOhFour__description">See if you find your way back on route:</p>
-  <div class="FourOhFour_nav"></div>
+  <div class="FourOhFour__container">
+    <h1 class="FourOhFour__title">Page Not Found</h1>
+    <h2 class="FourOhFour__subtitle">Looks like you got a little lost.</h2>
+    <p class="FourOhFour__description">See if you can navigate your way back on track:</p>
+    <div class="FourOhFour__nav">
+      <Link class="FourOhFour__link" to="home">Home</Link>
+      <Link class="FourOhFour__link" to="About">About</Link>
+      <Link class="FourOhFour__link" to="seminars">Free Seminars</Link>
+      <Link class="FourOhFour__link" to="pricing">Pricing</Link>
+    </div>
+
+    <img class="FourOhFour__image" src="./assets/chart.jpg" alt="Navigation Chart">
+  </div>
 </section>
+
+<style lang="scss">
+  @import './404.scss';
+</style>
