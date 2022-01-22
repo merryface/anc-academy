a791f0600e200a5791f36b539735b03ade4502a3
Tarik <tarikmerrylees@gmail.com>
2022-01-22 10:37:53 +0000

    404 routing bugs fixed

--- src/routes/404.svelte
+++ src/routes/404.svelte
@@ -8,8 +8,8 @@ import { Link } from "svelte-routing";
     <h2 class="FourOhFour__subtitle">Looks like you got a little lost.</h2>
     <p class="FourOhFour__description">See if you can navigate your way back on track:</p>
     <div class="FourOhFour__nav">
-      <Link class="FourOhFour__link" to="home">Home</Link>
-      <Link class="FourOhFour__link" to="About">About</Link>
+      <Link class="FourOhFour__link" to="/">Home</Link>
+      <Link class="FourOhFour__link" to="about">About</Link>
       <Link class="FourOhFour__link" to="seminars">Free Seminars</Link>
       <Link class="FourOhFour__link" to="pricing">Pricing</Link>
     </div>
