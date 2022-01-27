98dd9ed4f585b0da740267a642e6c39bddb5fa4e
Tarik <tarikmerrylees@gmail.com>
2022-01-27 11:30:26 +0000

    feature/beeokings-hero: added hero image

--- src/components/Book/BookHero/BookHero.svelte
+++ src/components/Book/BookHero/BookHero.svelte
@@ -1,9 +1,12 @@
-<script></script>
+<script>
+  import { content } from './BookHero.js'
+</script>
 
 <section class="BookingHero">
   <div class="BookingHero__container">
     <p class="AboutHero__text">
+      {content.title}
     </p>
-    <img src={""} alt={""} class="BookingHero__image">
   </div>
+  <img src={content.image.url} alt={content.image} class="BookingHero__image">
 </section>
