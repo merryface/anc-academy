bc8d592a8bfbaf090a39da2c2bd73559d7391033
Tarik <tarikmerrylees@gmail.com>
2022-01-27 19:13:29 +0000

    feature/BookSeminarCalendar: calendar added but bugs

--- src/components/Book/SeminarCalendar/SeminarCalendar.js
+++ src/components/Book/SeminarCalendar/SeminarCalendar.js
@@ -1,4 +1,4 @@
 export const content  = {
   title: "Get a seminar slot",
-  textBody: "Interested in getting a taster of what we teach? Use the calendar below to book a slot at the next available seminar."
+  textBody: "Interested in getting a taster of what we teach? Use the calendar below to book a slot at the next available seminar. Times shown in UK time zone."
 }
