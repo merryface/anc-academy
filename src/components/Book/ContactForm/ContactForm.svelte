2e6c4cae95e66a7c126d992a2197dc3ccae58f3b
Tarik <tarikmerrylees@gmail.com>
2022-01-27 22:46:17 +0000

    feature/book-contactform: basic formating added

---
+++ src/components/Book/ContactForm/ContactForm.svelte
@@ -0,0 +1,15 @@
+<script>
+  import {content} from './ContactForm.js'
+</script>
+
+<section class="ContactForm">
+  <div class="ContactForm__container">
+    <h2 class="ContactForm__title">{content.title}</h2>
+  </div>
+
+  <form action="https://formsubmit.co/tarikmerrylees@gmail.com" method="POST">
+    <input type="text" name="name" required>
+    <input type="email" name="email" required>
+    <button type="submit">Send</button>
+</form>
+</section>
