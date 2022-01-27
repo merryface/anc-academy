686f8eb5ebfd83521c34624b844b6690a6163fe3
Tarik <tarikmerrylees@gmail.com>
2022-01-27 19:13:29 +0000

    feature/BookSeminarCalendar: calendar added but bugs

--- src/components/Book/SeminarCalendar/SeminarCalendar.svelte
+++ src/components/Book/SeminarCalendar/SeminarCalendar.svelte
@@ -5,11 +5,13 @@
 <section class="SeminarCalendar">
   <div class="SeminarCalendar__container">
     <div class="SeminarCalendar__text">
-      <h2 class="title">{content.title}</h2>
+      <h2 class="SeminarCalendar__title">{content.title}</h2>
       <p class="SeminarCalendar__textBody">{content.textBody}</p>
     </div>
-    <iframe class="SeminarCalendar__calendar" title="ANC Academy Calendar" src="https://calendar.google.com/calendar/embed?src=uoof8pemte4fpo86v6vo67uqv8%40group.calendar.google.com&ctz=Europe%2FLondon" style="border: 0" width="100%" height="600" frameborder="0" scrolling="no"></iframe>
+    <iframe SameSite=None class="SeminarCalendar__calendar" title="ANC Academy Calendar" src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=Europe%2FLondon&&showTz=0showCalendars=0&showPrint=0&showDate=1&showTitle=0&title&showTabs=0&src=dW9vZjhwZW10ZTRmcG84NnY2dm82N3VxdjhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%237986CB" scrolling="yes"></iframe>
   </div>
 </section>
 
-<style lang="scss"></style>
+<style lang="scss">
+  @import './SeminarCalendar.scss';
+</style>
