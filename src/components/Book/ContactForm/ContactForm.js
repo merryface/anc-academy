f9de0d9011809c68b74006682c3ee992ac17bd0f
Tarik <tarikmerrylees@gmail.com>
2022-01-28 09:10:58 +0000

    feature/Book-contactForm: form working and styled for mobile

--- src/components/Book/ContactForm/ContactForm.js
+++ src/components/Book/ContactForm/ContactForm.js
@@ -1,3 +1,7 @@
 export const content = {
   title: "Get in Touch",
+  text: "Get in touch and organise a lesson with us. Tell us what it is you are looking for and we’ll get you sorted out with a bespoke lesson. Otherwise, check out list of classes and packages below.",
+  action: "https://formsubmit.co/tarikmerrylees@gmail.com",
+  root: window.location.origin,
+  emailString: "e924a2d5b621e91baddb02ef2eebb792",
 }
--- src/components/Book/ContactForm/ContactForm.svelte
+++ src/components/Book/ContactForm/ContactForm.svelte
@@ -1,15 +1,38 @@
 <script>
   import {content} from './ContactForm.js'
+  import { onMount } from 'svelte';
+
+  onMount(async () => {
+    document.getElementById('sendTo').setAttribute("action", `https://formsubmit.co/${content.emailString}`);
+  });
 </script>
 
 <section class="ContactForm">
   <div class="ContactForm__container">
     <h2 class="ContactForm__title">{content.title}</h2>
-  </div>
+    <p class="ContactForm__text">{content.text}</p>
 
-  <form action="https://formsubmit.co/tarikmerrylees@gmail.com" method="POST">
-    <input type="text" name="name" required>
-    <input type="email" name="email" required>
-    <button type="submit">Send</button>
-</form>
+    <form class="ContactForm__form" id="sendTo" accept-charset="utf-8" method="POST">
+      <label class="ContactForm__formLabel" for="email">Enter Your Email Address</label>
+      <input class="ContactForm__formInput" type="email" name="email" placeholder="Email Address">
+
+      <label class="ContactForm__formLabel" for="_subject">Subject</label>
+      <input class="ContactForm__formInput" name="_subject" placeholder="I'd like to book a lesson">
+
+      <label class="ContactForm__formLabel" for="message">Message</label>
+      <textarea class="ContactForm__textArea" type="textarea" name="message" placeholder="Type your message here" required></textarea>
+
+      <button class="ContactForm__submit" type="submit">Submit</button>
+
+      <!-- hidden -->
+      <input type="hidden" name="_next" value={content.root + "/seminars"}>
+      <input type="text" name="_honey" style="display:none">
+      <input type="hidden" name="_template" value="table">
+      <input type="hidden" name="_autoresponse" value="your custom message">
+  </form>
+  </div>
 </section>
+
+<style lang="scss">
+  @import './contactForm';
+</style>
