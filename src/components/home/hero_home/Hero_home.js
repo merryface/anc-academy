1b1f924ed2e0fb191d831698412fa8402600ca7c
Tarik <tarikmerrylees@gmail.com>
2022-01-29 21:12:37 +0000

    temporarily reroute home anchors to pricing page

--- src/components/home/hero_home/Hero_home.js
+++ src/components/home/hero_home/Hero_home.js
@@ -3,12 +3,14 @@ export const videoUrl = "./assets/homeHeroVideo.mp4";
 export const ctaItems = [
   {
     title: "Passionate flight simmers wanting Simulator instruction",
-    url: "pricing-sim",
+    // url: "pricing-sim",
+    url: "pricing",
     linkLabel: "Simulator Instruction",
   },
   {
     title: "Pilots in training wanting supplemental online instruction",
-    url: "pricing-groundschool",
+    // url: "pricing-groundschool",
+    url: "pricing",
     linkLabel: "Online Tuition",
   },
 ]
