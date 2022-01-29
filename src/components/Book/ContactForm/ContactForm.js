00a309b8e3f25ac1d15d9838589bfb53c8ec924e
Tarik <tarikmerrylees@gmail.com>
2022-01-29 00:50:51 +0000

    feature?book-contactForm: styled and email received page styled

--- src/components/Book/ContactForm/ContactForm.js
+++ src/components/Book/ContactForm/ContactForm.js
@@ -3,5 +3,13 @@ export const content = {
   text: "Get in touch and organise a lesson with us. Tell us what it is you are looking for and we’ll get you sorted out with a bespoke lesson. Otherwise, check out list of classes and packages below.",
   action: "https://formsubmit.co/tarikmerrylees@gmail.com",
   root: window.location.origin,
+  redirect: "/emailSent",
   emailString: "e924a2d5b621e91baddb02ef2eebb792",
+  responseMessage: `
+    Thank you for getting in Touch!
+    \n
+    An instructor will get back to you as quickly as possible.
+    \n
+    The ANC Academy Team
+  `,
 }
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,7 +5,8 @@
   import NotFound from './404.svelte';
   import Seminars from '../components/Seminars/Seminars.svelte';
   import Pricing from '../components/Pricing/Pricing.svelte';
-import Book from '../components/Book/Book.svelte';
+  import Book from '../components/Book/Book.svelte';
+  import EmailSent from '../components/Book/ContactForm/EmailSent.svelte';
   export let url = '';
 </script>
 
@@ -15,5 +16,7 @@ import Book from '../components/Book/Book.svelte';
   <Route path="pricing" component={Pricing} />
   <Route path="seminars" component={Seminars} />
   <Route path="book" component={Book} />
+
+  <Route path="emailSent" component={EmailSent} />
   <Route component={NotFound} />
 </Router>
