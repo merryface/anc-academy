635b2174027bfc348d1c9d9a5b36b5230ebd5610
Tarik <tarikmerrylees@gmail.com>
2022-01-29 20:23:57 +0000

    feature/joinUs: content added

---
+++ src/components/JoinUs/JoinUs.js
@@ -0,0 +1,32 @@
+export const content = {
+  title: "So you want to share the passion?",
+  intro: "That’s awesome! We really want to add new members to our team, but we’re not quite there yet. We’re not far however, so, if you’re still interested in joining us, here are our criteria for us to consider you:",
+  requirements: [
+    {
+      header: "Non Commercial Pilots or Instructors",
+      points: [
+        "Hold an ICAO Pilot License",
+        "Have at least 2 years experience using at least 2 different home flight simulators",
+        "Demonstrate a solid understanding of fundamentals at the ATP/ATPL level",
+        "Ability to communicate clearly and effectively",
+      ]
+    },
+    {
+      header: "Professional Pilot",
+      points: [
+        "Hold an ICAO ATPL or CPL",
+        "Have at least 3 months of experience using a flight simulator and have access to at least 2 flight simulators",
+        "Ability to communicate clearly and effectively aviation concepts",
+      ]
+    },
+    {
+      header: "Flight Instructors",
+      points: [
+        "Hold an ICAO ATPL or CPL with a Flight Instructor rating (currency not required)",
+        "Have at least 3 months of experience using a flight simulator and have access to at least 2 flight simulators",
+      ]
+    },
+  ],
+  nonPilots: "We would love to consider capable non-pilot candidates but cannot offer them an opportunity at this time.",
+  contactUs: "If you would like to get in touch for future consideration, please contact us using the contact form. "
+}
