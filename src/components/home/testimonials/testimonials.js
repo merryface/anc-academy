99f35230e260c2c318055effa813c374fe595801
Tarik <tarikmerrylees@gmail.com>
2022-02-05 04:36:21 +0000

    added mikes review

--- src/components/home/testimonials/testimonials.js
+++ src/components/home/testimonials/testimonials.js
@@ -1,15 +1,21 @@
 export const testimonials = [
   {
     id: 0,
+    text: "ANC's sessions have helped me discover a passion for flying and flight simulators I didn't know I had. Starting from scratch, they taught me how to operate an aircraft using proper procedures. Professional teaching and constant feedback from their amazing and friendly instructors in group and one-on-one sessions, have taught me how to avoid the pitfalls of uneducated simulator flying, and gave me a much steeper learning curve than I could have hoped for. I’m grateful for everything I learned here when I go to a real flight school.",
+    name: "Martin",
+    title: "Aerospace Engineering Student"
+  },
+  {
+    id: 1,
     text: "ANC Academy's seminars are incredible. If you spend any time with YouTube tutorials or blog posts, ANC is the absolute next level. Group classes, seminars, and one-on-one instruction have all helped me immensely and given me the confidence to find a real-world flight school near me.",
     name: "Null",
     title: "Competitive Gamer"
   },
   {
-    id: 1,
-    text: "ANC's sessions have helped me discover a passion for flying and flight simulators I didn't know I had. Starting from scratch, they taught me how to operate an aircraft using proper procedures. Professional teaching and constant feedback from their amazing and friendly instructors in group and one-on-one sessions, have taught me how to avoid the pitfalls of uneducated simulator flying, and gave me a much steeper learning curve than I could have hoped for. I’m grateful for everything I learned here when I go to a real flight school.",
-    name: "Martin",
-    title: "Aerospace Engineering Student"
+    id: 2,
+    text: "Here's your Masterclass right here! Highly knowledgable about the subject with the ability to apply it to his real world experiences. Illustrates concepts and principles with stories and demonstrations which really brings some of the more abstract aviation concepts to life. Does all this in a highly professional yet personable manner. Really easy going but manages to maintain the focus of the lesson plan without making anything seem rushed or hurried. Obviously understands how to help students learn.",
+    name: "Mike",
+    title: "Doctor and future pilot"
   },
 ]
 
