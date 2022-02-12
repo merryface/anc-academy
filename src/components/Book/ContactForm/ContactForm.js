72826ca4c379223915195dbf89ac0b29f7fc27e9
Tarik <tarikmerrylees@gmail.com>
2022-02-12 12:38:27 +0000

    changed email

--- src/components/Book/ContactForm/ContactForm.js
+++ src/components/Book/ContactForm/ContactForm.js
@@ -1,7 +1,7 @@
 export const content = {
   title: "Get in Touch",
   text: "Get in touch and organise a lesson with us. Tell us what it is you are looking for and we’ll get you sorted out with a bespoke lesson. Otherwise, check out list of classes and packages below.",
-  action: "https://formsubmit.co/tarikmerrylees@gmail.com",
+  action: "https://formsubmit.co/e924a2d5b621e91baddb02ef2eebb792",
   root: window.location.origin,
   redirect: "/emailSent",
   emailString: "e924a2d5b621e91baddb02ef2eebb792",
