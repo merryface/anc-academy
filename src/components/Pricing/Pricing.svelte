1e92e5bfbf7115b0b1c15ab1a105892cc692d5d9
Tarik <tarikmerrylees@gmail.com>
2022-02-22 13:54:40 +0000

    page link component created

--- src/components/Pricing/Pricing.svelte
+++ src/components/Pricing/Pricing.svelte
@@ -1,10 +1,8 @@
 <script>
   import PricingHero from "./PricingHero/PricingHero.svelte";
-  import Rates from "./Rates/Rates.svelte";
   import TextContent from "./TextContent/TextContent.svelte";
 </script>
 
 <PricingHero />
-<Rates />
 <TextContent />
 
---
+++ src/components/Pricing/PricingPageLink/PricingPageLink.svelte
@@ -0,0 +1,12 @@
+<script>
+import { Link } from "svelte-routing";
+export let content;
+</script>
+
+<Link class="PricingPageLink" to={content.url}>
+  <img src={content.image.url} alt={content.image.alt} class="PricingPageLink__image">
+</Link>
+
+<style lang="scss">
+  @import './pricing-page-link';
+</style>
