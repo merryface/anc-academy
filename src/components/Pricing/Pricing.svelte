93fdf6e3cf457b5872ff0471c30aa61087850633
Tarik <tarikmerrylees@gmail.com>
2022-02-23 22:57:43 +0000

    pricing top text added and styled

--- src/components/Pricing/Pricing.svelte
+++ src/components/Pricing/Pricing.svelte
@@ -1,10 +1,12 @@
 <script>
   import PricingHero from "./PricingHero/PricingHero.svelte";
-  import TextContent from "./TextContent/TextContent.svelte";
+  import Summary from "./Summary/Summary.svelte";
   import PricingPageLinks from "./PricingPageLinks/PricingPageLinks.svelte";
+  import TextContent from "./TextContent/TextContent.svelte";
 </script>
 
 <PricingHero />
+<Summary />
 <PricingPageLinks />
 <TextContent />
 
---
+++ src/components/Pricing/Summary/Summary.svelte
@@ -0,0 +1,11 @@
+<section class="PricingSummary">
+  <div class="PricingSummary__container">
+    <p class="PricingSummary__text">
+      Our rates are based on the experience and qualifications of our instructors. We recommend speaking to us before buying a full syllabus. 
+    </p>
+  </div>
+</section>
+
+<style lang="scss">
+  @import './summary'
+</style>
