d00efb2e7ce2487ca5a5b0372d635da44c113877
Tarik <tarikmerrylees@gmail.com>
2022-02-23 10:32:49 +0000

    basic button styling

---
+++ src/components/Pricing/PricingPageLinks/PricingPageLinks.svelte
@@ -0,0 +1,19 @@
+<script>
+import PricingPageLink from "../PricingPageLink/PricingPageLink.svelte";
+</script>
+
+<PricingPageLink content = {{
+  url: "pricing-simulator",
+  imageUrl: './assets/bookNowHero.jpg',
+  // imageAlt: "alt text",
+  label: "Online Flight Simulator Training",
+  sublabel: "Learn how to fly using your own flight simulator remotely"
+}} />
+
+<PricingPageLink content = {{
+  url: "pricing-groundschool",
+  imageUrl: './assets/chart.jpg',
+  // imageAlt: "alt text",
+  label: "Online Groundschool Training",
+  sublabel: "Get realtime instruction remotely"
+}} />
