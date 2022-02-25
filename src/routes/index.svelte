5b4e1b362357fe4a2775f14524e625d52964d0c7
Tarik <tarikmerrylees@gmail.com>
2022-02-25 23:08:06 +0000

    hero added and added style for mobile

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,6 +5,7 @@
   import NotFound from './404.svelte';
   import Seminars from '../components/Seminars/Seminars.svelte';
   import Pricing from '../components/Pricing/Pricing.svelte';
+  import PricingPage from '../components/PricingPage/PricingPage.svelte';
   import Book from '../components/Book/Book.svelte';
   import EmailSent from '../components/Book/ContactForm/EmailSent.svelte';
   import Terms from '../components/Terms/Terms.svelte';
@@ -15,9 +16,11 @@
 <Router {url}>
   <Route path="/" component={Home} />
   <Route path="about" component={About} />
-  <Route path="pricing" component={Pricing} />
   <Route path="seminars" component={Seminars} />
   <Route path="book" component={Book} />
+  
+  <Route path="pricing" component={Pricing} />
+  <Route path="pricing-simulator" component={PricingPage} />
 
   <Route path="emailSent" component={EmailSent} />
   <Route path="terms-and-conditions" component={Terms} />
