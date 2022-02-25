c310ffcad9619a3c163e7fe6552cc7f08e813de8
Tarik <tarikmerrylees@gmail.com>
2022-02-25 23:08:06 +0000

    hero added and added style for mobile

---
+++ src/components/PricingPage/PricingPage.svelte
@@ -0,0 +1,12 @@
+<script>
+import PricingPageHero from "./pageComponents/PricingPageHero/PricingPageHero.svelte";
+
+</script>
+
+<PricingPageHero content= {{
+  heading: "Online Flight Simulator Instruction",
+  subtitle: "Receive hands-on flight training from the comfort of your home",
+  imageUrl: "./assets/bookNowHero.jpg",
+  imageAlt: "Image Alt",
+  linkLabel: "Book Now"
+}} />
