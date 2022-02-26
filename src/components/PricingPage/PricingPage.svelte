0995877fa9029c5b3fbd2ec162a39d2a23df3a99
Tarik <tarikmerrylees@gmail.com>
2022-02-26 11:53:25 +0000

    added midpage banner content

--- src/components/PricingPage/PricingPage.svelte
+++ src/components/PricingPage/PricingPage.svelte
@@ -1,6 +1,7 @@
 <script>
   import PricingPageHero from "./pageComponents/PricingPageHero/PricingPageHero.svelte";
   import Rates from "./pageComponents/Rates/Rates.svelte";
+  import MidPageBanner from "./pageComponents/MidPageBanner/MidPageBanner.svelte";  
   import { simulatorRates } from './pageComponents/Rates/simulatorRates';
 </script>
 
@@ -13,3 +14,8 @@
 }} />
 
 <Rates content={simulatorRates} />
+<MidPageBanner content={{
+  imageUrl: './assets/da62CockpitBanner.jpg',
+  imageAlt: "Da62 Cockpit",
+  text: "We also provide <span>full courses</span>, not just one off experiences"
+}} />
