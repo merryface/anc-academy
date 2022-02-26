df7fe1904de5b3d15718f00e5407112e5c6f9cb4
Tarik <tarikmerrylees@gmail.com>
2022-02-26 11:53:25 +0000

    added midpage banner content

---
+++ src/components/PricingPage/pageComponents/MidPageBanner/MidPageBanner.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let content;
+</script>
+
+<section class="MidPageBanner">
+  <img src={content.imageUrl} alt={content.imageAlt} class="MidPageBanner__banner">
+  <div class="MidPageBanner__container">
+    <p class="MidPageBanner__text">
+      {@html content.text}
+    </p>
+  </div>
+</section>
