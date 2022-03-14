cb56fab5d572995e2a747ca92e14e8b596a2bd6d
Tarik <tarikmerrylees@gmail.com>
2022-03-14 09:27:16 +0000

    start styling accordion

---
+++ src/components/PricingPage/pageComponents/Accordion/Accordion.svelte
@@ -0,0 +1,29 @@
+<script>
+  export let content;
+  import FaAngleDown from 'svelte-icons/fa/FaAngleDown.svelte'
+  import FaFilePdf from 'svelte-icons/fa/FaFilePdf.svelte'
+</script>
+
+<section class="Accordion">
+  <div class="Accordion__container">
+    <div class="Accordion__header">
+      <div class="Accordion__headerContainer">
+        <h3 class="Accordion__text">{content.header}</h3>
+        <div class="Accordion__button"><FaAngleDown/></div>
+      </div>
+    </div>
+
+    <div class="Accordion__items">
+      {#each content.items as {label, fileUrl}}
+      <div class="Accordion__item">
+        <p class="Accordion__itemName">{label}</p>
+        <a href={fileUrl} class="Accordion__download"><FaFilePdf /></a>
+      </div>
+      {/each}
+    </div>
+  </div>
+</section>
+
+<style lang="scss">
+  @import "./accordion";
+</style>
