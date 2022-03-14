e286e6458d3f713cc07241d037af3bd6b3ee3854
Tarik <tarikmerrylees@gmail.com>
2022-03-14 22:47:56 +0000

    toggle switch transition working

--- src/components/PricingPage/pageComponents/Accordion/Accordion.svelte
+++ src/components/PricingPage/pageComponents/Accordion/Accordion.svelte
@@ -2,6 +2,11 @@
   export let content;
   import FaAngleDown from 'svelte-icons/fa/FaAngleDown.svelte'
   import FaFilePdf from 'svelte-icons/fa/FaFilePdf.svelte'
+
+  let isOpen = false
+  const toggle = () => isOpen = !isOpen
+  $: isExpanded = isOpen ? " expanded" : ""
+  $: indicateExpanded = isOpen ? " opened" : " closed"
 </script>
 
 <section class="Accordion">
@@ -9,11 +14,11 @@
     <div class="Accordion__header">
       <div class="Accordion__headerContainer">
         <h3 class="Accordion__text">{content.header}</h3>
-        <div class="Accordion__button icon"><FaAngleDown/></div>
+        <div class="Accordion__button icon{indicateExpanded}" on:click={toggle}><FaAngleDown/></div>
       </div>
     </div>
 
-    <div class="Accordion__items">
+    <div class="Accordion__items{isExpanded}">
       {#each content.items as {label, fileUrl}}
       <div class="Accordion__item">
         <p class="Accordion__itemName">{label}</p>
