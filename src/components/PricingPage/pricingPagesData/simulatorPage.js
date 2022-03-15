7eaab7f43b678e048fff7876b7b23332c8ab049c
Tarik <tarikmerrylees@gmail.com>
2022-03-15 22:55:41 +0000

    changed to sim page and refactoring index

---
+++ src/components/PricingPage/pricingPagesData/simulatorPage.js
@@ -0,0 +1,53 @@
+export const simulatorPage = {
+  hero: {
+    heading: "Online Flight Simulator Instruction",
+    subtitle: "Receive hands-on flight training from the comfort of your home",
+    imageUrl: "./assets/bookNowHero.jpg",
+    imageAlt: "Image Alt",
+    linkLabel: "Book Now"
+  },
+  midPageBanner: {
+    imageUrl: './assets/da62CockpitBanner.jpg',
+    imageAlt: "Da62 Cockpit",
+    text: `We also provide <span class="MidPageBanner__blueText">full courses</span>, not just one off experiences`
+  },
+  trainingPackages: {
+    summary: "If you are ready to commit to a full training course, you can select one of the packages below to fully immerse yourself in a full training course experience.",
+    packages: [
+      {
+        header: "Simulator Private Pilot Training",
+        items: [
+          {
+          label: "PPL Phase 1",
+          fileUrl: "/",
+        },
+        {
+          label: "PPL Phase 2",
+          fileUrl: "/",
+        },
+        {
+          label: "PPL Phase 3",
+          fileUrl: "/",
+        },
+        {
+          label: "PPL Phase 4",
+          fileUrl: "/",
+        },
+        {
+          label: "Full PPL Course",
+          fileUrl: "/",
+        },
+      ]
+      },
+      {
+        header: "Simulator Multi-Engine Training",
+          items: [
+            {
+            label: "Multi-Engine Course",
+            fileUrl: "/",
+          }
+        ]
+      }
+    ]
+  }
+}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,8 +4,10 @@
   import About from '../components/About/About.svelte';
   import NotFound from './404.svelte';
   import Seminars from '../components/Seminars/Seminars.svelte';
+  
   import Pricing from '../components/Pricing/Pricing.svelte';
-  import PricingPage from '../components/PricingPage/PricingPage.svelte';
+  import SimulatorPricingPage from '../components/PricingPage/SimulatorPricingPage.svelte';
+  
   import Book from '../components/Book/Book.svelte';
   import EmailSent from '../components/Book/ContactForm/EmailSent.svelte';
   import Terms from '../components/Terms/Terms.svelte';
@@ -14,17 +16,17 @@
 </script>
 
 <Router {url}>
-  <Route path="/" component={Home} />
-  <Route path="about" component={About} />
+  <Route path="/"><Home/></Route>
+  <Route path="about" ><About /></Route>
   <Route path="seminars" component={Seminars} />
   <Route path="book" component={Book} />
   
   <Route path="pricing" component={Pricing} />
-  <Route path="pricing-simulator" component={PricingPage} />
+  <Route path="pricing-simulator"><SimulatorPricingPage /></Route>
 
   <Route path="emailSent" component={EmailSent} />
-  <Route path="terms-and-conditions" component={Terms} />
-  <Route path="join-us" component={JoinUs} />
+  <Route path="terms-and-conditions"><Terms /></Route>
+  <Route path="join-us"><JoinUs /></Route>
 
   <Route component={NotFound} />
 </Router>
