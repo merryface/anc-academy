64bbd3b63f63ac1a6b870b96f3d8c5ecb681e3db
Tarik <tarikmerrylees@gmail.com>
2022-03-15 15:57:48 +0000

    accordion collapse animation works

--- src/components/PricingPage/pageComponents/Accordion/Accordion.svelte
+++ src/components/PricingPage/pageComponents/Accordion/Accordion.svelte
@@ -1,5 +1,6 @@
 <script>
   export let content;
+  import { slide } from "svelte/transition";
   import FaAngleDown from 'svelte-icons/fa/FaAngleDown.svelte'
   import FaFilePdf from 'svelte-icons/fa/FaFilePdf.svelte'
 
@@ -18,14 +19,16 @@
       </div>
     </div>
 
-    <div class="Accordion__items{isExpanded}">
+    <ul class="Accordion__items{isExpanded}">
+      {#if isOpen}
       {#each content.items as {label, fileUrl}}
-      <div class="Accordion__item">
-        <p class="Accordion__itemName">{label}</p>
-        <a href={fileUrl} class="Accordion__download icon"><FaFilePdf /></a>
-      </div>
-      {/each}
-    </div>
+        <li class="Accordion__item" transition:slide={{ duration: 300 }}>
+          <p class="Accordion__itemName">{label}</p>
+          <a href={fileUrl} class="Accordion__download icon"><FaFilePdf /></a>
+        </li>
+        {/each}
+      {/if}
+    </ul>
   </div>
 </section>
 
