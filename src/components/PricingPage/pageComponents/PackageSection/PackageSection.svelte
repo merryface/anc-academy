019ac5e1ca2b7a58bd76715436b3730cd61af699
Tarik <tarikmerrylees@gmail.com>
2022-03-15 16:48:47 +0000

    add package section to output several accordions

---
+++ src/components/PricingPage/pageComponents/PackageSection/PackageSection.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Accordion from "../Accordion/Accordion.svelte";
+  export let summary
+  export let accordionItems;
+</script>
+
+<section class="PackageSection">
+  <div class="PackageSection__section">
+    <p class="PackageSection__text">{ summary }</p>
+
+    {#each accordionItems as accordion}
+      <Accordion content = {accordion} />
+    {/each}
+  </div>
+</section>
