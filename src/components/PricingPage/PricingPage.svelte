58a01fa9b2abe6cc26ea110752293fcdecd9dd4c
Tarik <tarikmerrylees@gmail.com>
2022-03-16 08:12:45 +0000

    refactor to make pricing page reusable

---
+++ src/components/PricingPage/PricingPage.svelte
@@ -0,0 +1,17 @@
+<script>
+export let content;
+export let pricingInfo;  
+  
+  import Rates from "./pageComponents/Rates/Rates.svelte";
+  import PricingPageHero from "./pageComponents/PricingPageHero/PricingPageHero.svelte";
+  import MidPageBanner from "./pageComponents/MidPageBanner/MidPageBanner.svelte";  
+  import PackageSection from "./pageComponents/PackageSection/PackageSection.svelte";
+</script>
+
+<PricingPageHero content= {content.hero} />
+<Rates content ={pricingInfo} />
+<MidPageBanner content = {content.midPageBanner} />
+<PackageSection
+  summary = {content.trainingPackages.summary}
+  accordionItems = {content.trainingPackages.packages}
+/>
