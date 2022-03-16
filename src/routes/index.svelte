fc9b517b99b8271cfd7270c210f46ccb0e911cbc
Tarik <tarikmerrylees@gmail.com>
2022-03-16 08:12:45 +0000

    refactor to make pricing page reusable

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,15 @@
 <script>
   import { Router, Route } from 'svelte-routing';
+  import { simulatorPage } from "../components/PricingPage/pricingPagesData/simulatorPage"
+  import { simulatorRates } from '../components/PricingPage/pricingPagesData/pricingData/simulatorRates';
+
   import Home from "../components/home/Home.svelte";
   import About from '../components/About/About.svelte';
   import NotFound from './404.svelte';
   import Seminars from '../components/Seminars/Seminars.svelte';
   
   import Pricing from '../components/Pricing/Pricing.svelte';
-  import SimulatorPricingPage from '../components/PricingPage/SimulatorPricingPage.svelte';
+  import PricingPage from '../components/PricingPage/PricingPage.svelte';
   
   import Book from '../components/Book/Book.svelte';
   import EmailSent from '../components/Book/ContactForm/EmailSent.svelte';
@@ -22,7 +25,7 @@
   <Route path="book"><Book /></Route>
   
   <Route path="pricing"><Pricing /></Route>
-  <Route path="pricing-simulator"><SimulatorPricingPage /></Route>
+  <Route path="pricing-simulator"><PricingPage content={simulatorPage} pricingInfo={simulatorRates} /></Route>
 
   <Route path="emailSent"><EmailSent /></Route>
   <Route path="terms-and-conditions"><Terms /></Route>
