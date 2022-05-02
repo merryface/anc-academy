dee13dcea7fc345a033a6979136a9733452ed8b9
Tarik <tarikmerrylees@gmail.com>
2022-05-02 11:56:08 +0100

    fix broken link

--- src/components/PricingPage/pageComponents/PricingPageHero/PricingPageHero.svelte
+++ src/components/PricingPage/pageComponents/PricingPageHero/PricingPageHero.svelte
@@ -8,7 +8,7 @@
     <div class="PricingPageHero__text">
       <h1 class="PricingPageHero__heading">{content.heading}</h1>
       <p class="PricingPageHero__subtitle">{content.subtitle}</p>
-      <Link class="PricingPageHero__link" to="Book">{content.linkLabel}</Link>
+      <Link class="PricingPageHero__link" to="book">{content.linkLabel}</Link>
     </div>
     <img src={content.imageUrl} alt={content.imageAlt} class="PricingPageHero__image">
   </div>
