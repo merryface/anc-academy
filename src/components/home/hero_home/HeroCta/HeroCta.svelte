4cf5cb18c273ff5e862d0f0d3287f2dca75a7150
Tarik <tarikmerrylees@gmail.com>
2022-10-22 21:49:38 +0100

    restyle layout for cta

--- src/components/home/hero_home/HeroCta/HeroCta.svelte
+++ src/components/home/hero_home/HeroCta/HeroCta.svelte
@@ -2,16 +2,13 @@
   export let title;
   export let linkLabel;
   export let url;
-  import IoIosAirplane from 'svelte-icons/io/IoIosAirplane.svelte'
   import { Link } from 'svelte-routing';
 </script>
 
 <div class="HeroCta__ctaItem">
   <p class="HeroCta__title">{title}</p>
-  <Link to={url} class="HeroCta__link">{linkLabel}
-    <div class="HeroCta__icon">
-      <IoIosAirplane />
-    </div>
+  <Link to={url} class="HeroCta__link">
+    {linkLabel}
   </Link>
 </div>
 
