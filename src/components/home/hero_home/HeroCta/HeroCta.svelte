ebbbf5a55fafd4cb240dc338a72fb0ae3cd809f3
Tarik <tarikmerrylees@gmail.com>
2022-10-22 19:14:23 +0100

    create and style home cta item

---
+++ src/components/home/hero_home/HeroCta/HeroCta.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let title;
+  export let linkLabel;
+  export let url;
+  import IoIosAirplane from 'svelte-icons/io/IoIosAirplane.svelte'
+  import { Link } from 'svelte-routing';
+</script>
+
+<div class="HeroCta__ctaItem">
+  <p class="HeroCta__title">{title}</p>
+  <Link to={url} class="HeroCta__link">{linkLabel}
+    <div class="HeroCta__icon">
+      <IoIosAirplane />
+    </div>
+  </Link>
+</div>
+
+<style lang="scss">
+  @import "./hero_cta.scss";
+</style>
--- src/components/home/hero_home/Hero_home.svelte
+++ src/components/home/hero_home/Hero_home.svelte
@@ -2,6 +2,7 @@
   import IoIosAirplane from 'svelte-icons/io/IoIosAirplane.svelte'
   import { Link } from 'svelte-routing';
   import { videoUrl, ctaItems, mobileHeroUrl, logo } from './Hero_home';
+  import HeroCta from './HeroCta/HeroCta.svelte';
 </script>
 
 <section class="HeroHome">
@@ -19,17 +20,10 @@
   <div class="HeroHome__container">
     <div class="HeroHome__cta">
       {#each ctaItems as {title, url, linkLabel}}
-        <div class="HeroHome__ctaItem">
-          <h2 class="HeroHome__title">{title}</h2>
-          <Link to={url} class="HeroHome__link">{linkLabel}
-            <div class="HeroHome__icon">
-              <IoIosAirplane />
-            </div>
-          </Link>
-        </div>
+      <HeroCta title={title} url={url} linkLabel={linkLabel} />
       {/each}
     </div>
-  </div>  
+  </div>
 </section>
 
 <style lang="scss">
