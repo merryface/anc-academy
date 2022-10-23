0a1eeb9260a10e4661fc275d27b1f92a9b60d68c
Tarik <tarikmerrylees@gmail.com>
2022-10-23 16:49:04 +0100

    move pricingpage to new directory

---
+++ src/pages/PricingPage.svelte
@@ -0,0 +1,17 @@
+<script>
+export let content;
+export let pricingInfo;  
+  
+  import Rates from "../components/PricingPage/pageComponents/Rates/Rates.svelte";
+  import PricingPageHero from "../components/PricingPage/pageComponents/PricingPageHero/PricingPageHero.svelte";
+  import MidPageBanner from "../components/PricingPage/pageComponents/MidPageBanner/MidPageBanner.svelte";  
+  import PackageSection from "../components/PricingPage/pageComponents/PackageSection/PackageSection.svelte";
+</script>
+
+<PricingPageHero content= {content.hero} />
+<Rates content ={pricingInfo} />
+<MidPageBanner content = {content.midPageBanner} />
+<PackageSection
+  summary = {content.trainingPackages.summary}
+  accordionItems = {content.trainingPackages.packages}
+/>
