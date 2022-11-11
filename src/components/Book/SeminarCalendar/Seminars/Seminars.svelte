ea61b381e4c8d0c01b879250125cf809b59423db
Tarik <tarikmerrylees@gmail.com>
2022-11-11 18:20:09 +0000

    move seminar details to content

--- src/components/Book/SeminarCalendar/Seminars/Seminars.svelte
+++ src/components/Book/SeminarCalendar/Seminars/Seminars.svelte
@@ -4,15 +4,17 @@
 
 <section class="Seminars">
   <div class="Seminars__container">
-    <h3 class="Seminars__seminarTitle">VFR Flying Introduction</h3>
+    {#each content.sems as sem}
+    <h3 class="Seminars__seminarTitle">{sem.title}</h3>
     <div class="Seminars__row">
       <p class="Seminars__label">Date & time:</p>
-      <p class="Seminars__dateTime">19th November 2022 at 20:00 UTC</p>
+      <p class="Seminars__dateTime">{sem.dateTime}</p>
     </div>
     <div class="Seminars__row">
       <p class="Seminars__label">Flight Instructor:</p>
-      <p class="Seminars__instructor">Puffmac</p>
+      <p class="Seminars__instructor">{sem.instructor}</p>
     </div>
+    {/each}
   </div>
   <a href={content.discord.url} target="_blank" class="Seminars__discord">
     {content.discord.label}
--- src/components/Book/SeminarCalendar/Seminars/seminars.js
+++ src/components/Book/SeminarCalendar/Seminars/seminars.js
@@ -2,5 +2,12 @@ export const content = {
   discord: {
     url: "https://discord.gg/scE5pGbBuE",
     label: "Join us on Discord to Participate in Seminars"
-  }
+  },
+  sems: [
+    {
+      title: "VFR Flying Introduction",
+      dateTime: "19th November 2022 at 20:00 UTC",
+      instructor: "Puffmac"
+    },
+  ]
 }
