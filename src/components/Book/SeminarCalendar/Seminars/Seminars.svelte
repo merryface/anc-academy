afc417a83d46a0d529669dd9eee0e6961ca4c6ec
Tarik <tarikmerrylees@gmail.com>
2022-11-11 14:29:33 +0000

    set up component

---
+++ src/components/Book/SeminarCalendar/Seminars/Seminars.svelte
@@ -0,0 +1,17 @@
+<script>
+  
+</script>
+
+<section class="Seminars">
+  <div class="Seminars__container">
+    <h3 class="Seminars__seminarTitle">VFR Flying Introduction</h3>
+    <p class="Seminars__dateTimeLabel">Date & time:</p>
+    <p class="Seminars__dateTime">19th November 2022 at 20:00 UTC</p>
+    <p class="Seminars__instructorLabel">Flight Insructor:</p>
+    <p class="Seminars__instructor">Puffmac</p>
+  </div>
+</section>
+
+<style lang="scss">
+  @import "./seminars.scss";
+</style>
