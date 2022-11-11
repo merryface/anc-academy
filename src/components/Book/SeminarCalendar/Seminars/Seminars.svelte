b4abd4faa31ad2dd8511a2c7e1524143ace06908
Tarik <tarikmerrylees@gmail.com>
2022-11-11 14:54:21 +0000

    add discord link

--- src/components/Book/SeminarCalendar/Seminars/Seminars.svelte
+++ src/components/Book/SeminarCalendar/Seminars/Seminars.svelte
@@ -1,15 +1,22 @@
 <script>
-  
+
 </script>
 
 <section class="Seminars">
   <div class="Seminars__container">
     <h3 class="Seminars__seminarTitle">VFR Flying Introduction</h3>
-    <p class="Seminars__dateTimeLabel">Date & time:</p>
-    <p class="Seminars__dateTime">19th November 2022 at 20:00 UTC</p>
-    <p class="Seminars__instructorLabel">Flight Insructor:</p>
-    <p class="Seminars__instructor">Puffmac</p>
+    <div class="Seminars__row">
+      <p class="Seminars__label">Date & time:</p>
+      <p class="Seminars__dateTime">19th November 2022 at 20:00 UTC</p>
+    </div>
+    <div class="Seminars__row">
+      <p class="Seminars__label">Flight Insructor:</p>
+      <p class="Seminars__instructor">Puffmac</p>
+    </div>
   </div>
+  <a href="https://discord.gg/scE5pGbBuE" target="_blank" class="Seminars__discord">
+    Join us on Discord to Participate in Seminars
+  </a>
 </section>
 
 <style lang="scss">
