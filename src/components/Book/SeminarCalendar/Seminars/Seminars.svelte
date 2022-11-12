6c9582808dc487bf68c029b718ef9950cd5318b8
Tarik <tarikmerrylees@gmail.com>
2022-11-12 21:12:29 +0000

    implement hard coded calendar event

--- src/components/Book/SeminarCalendar/Seminars/Seminars.svelte
+++ src/components/Book/SeminarCalendar/Seminars/Seminars.svelte
@@ -1,26 +1,14 @@
 <script>
   import {content} from './seminars'
-  import {onMount} from 'svelte'
-  import Airtable from 'Airtable'
-
   import Seminar from './Seminar/Seminar.svelte'
 
-  const api_key = process.env.API_KEY
-  const api_base = process.env.API_BASE
-  const base = new Airtable({apiKey: api_key}).base(api_base)
-  const table = base('Seminars')
-  let sems = []
 
-  onMount(async () => {
-    const records = await table.select().all()
-    sems = records
-  })
 </script>
 
 <section class="Seminars">
   <div class="Seminars__container">
-    {#each sems as sem}
-      <Seminar sem={sem.fields} />
+    {#each content.sems as sem}
+      <Seminar sem={sem} />
     {/each}
   </div>
   <a href={content.discord.url} target="_blank" class="Seminars__discord">
--- src/components/Book/SeminarCalendar/Seminars/seminars.js
+++ src/components/Book/SeminarCalendar/Seminars/seminars.js
@@ -2,5 +2,13 @@ export const content = {
   discord: {
     url: "https://discord.gg/scE5pGbBuE",
     label: "Join us on Discord to Participate in Seminars"
-  }
+  },
+
+  sems: [
+    {
+      Seminar: "VFR Flying Introduction",
+      Date: "Saturday 19th November 2022 at 20:00 - 22:00 UTC",
+      Instructor: "Puffmac"
+    }
+  ]
 }
