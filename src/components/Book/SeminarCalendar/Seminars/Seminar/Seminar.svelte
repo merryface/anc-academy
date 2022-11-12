29f2664415564d65c42d25b61ecf85a3c6bc4296
Tarik <tarikmerrylees@gmail.com>
2022-11-12 12:53:45 +0000

    make seminar component and fetch data

---
+++ src/components/Book/SeminarCalendar/Seminars/Seminar/Seminar.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let sem;
+</script>
+
+<h3 class="Seminar__seminarTitle">{sem.Seminar}</h3>
+<div class="Seminar__row">
+  <p class="Seminar__label">Date & time:</p>
+  <p class="Seminar__dateTime">{sem.Date}</p>
+</div>
+<div class="Seminar__row">
+  <p class="Seminar__label">Flight Instructor:</p>
+  <p class="Seminar__instructor">{sem.Instructor}</p>
+</div>
+
+<style lang="scss">
+  @import "./seminar.scss";
+</style>
--- src/components/Book/SeminarCalendar/Seminars/Seminars.svelte
+++ src/components/Book/SeminarCalendar/Seminars/Seminars.svelte
@@ -1,19 +1,27 @@
 <script>
   import {content} from './seminars'
+  import {onMount} from 'svelte'
+  import Airtable from 'Airtable'
+
+  import Seminar from './Seminar/Seminar.svelte'
+
+  const api_key = process.env.API_KEY
+  const api_base = process.env.API_BASE
+  const base = new Airtable({apiKey: api_key}).base(api_base)
+  const table = base('Seminars')
+  let sems = []
+
+  onMount(async () => {
+    const records = await table.select().all()
+    console.log(records)
+    sems = records
+  })
 </script>
 
 <section class="Seminars">
   <div class="Seminars__container">
-    {#each content.sems as sem}
-    <h3 class="Seminars__seminarTitle">{sem.title}</h3>
-    <div class="Seminars__row">
-      <p class="Seminars__label">Date & time:</p>
-      <p class="Seminars__dateTime">{sem.dateTime}</p>
-    </div>
-    <div class="Seminars__row">
-      <p class="Seminars__label">Flight Instructor:</p>
-      <p class="Seminars__instructor">{sem.instructor}</p>
-    </div>
+    {#each sems as sem}
+      <Seminar sem={sem.fields} />
     {/each}
   </div>
   <a href={content.discord.url} target="_blank" class="Seminars__discord">
