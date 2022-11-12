73d1427b969831dfe21f767ba3290cf922150bde
Tarik <tarikmerrylees@gmail.com>
2022-11-12 12:54:02 +0000

    move sems stuff into component

--- src/components/Book/SeminarCalendar/Seminars/seminars.js
+++ src/components/Book/SeminarCalendar/Seminars/seminars.js
@@ -2,12 +2,5 @@ export const content = {
   discord: {
     url: "https://discord.gg/scE5pGbBuE",
     label: "Join us on Discord to Participate in Seminars"
-  },
-  sems: [
-    {
-      title: "VFR Flying Introduction",
-      dateTime: "19th November 2022 at 20:00 UTC",
-      instructor: "Puffmac"
-    },
-  ]
+  }
 }
